ed93858468f391283b55acfec161063215d7549f
jongcher <jc1218a@naver.com>
2022-11-02 21:17:19 +0900

    초기생성
    초기생성

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FUCOS.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
