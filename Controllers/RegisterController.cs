9906e59d16a633a1d2398be194ef73ac05a03719
jongcher <jc1218a@naver.com>
2022-11-02 21:30:39 +0900

    메인페이지 링크 추가
    링크추가

---
+++ Controllers/RegisterController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FUCOS.Controllers
+{
+    public class RegisterController : Controller
+    {
+
+
+
+        [HttpGet]
+        [Route("/Register/Register")]
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
