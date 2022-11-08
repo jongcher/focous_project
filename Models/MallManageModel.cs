c03ffa3e65b87e00458c6ec48dc98296d13da523
jongcher <jc1218a@naver.com>
2022-11-08 20:11:36 +0900

    123123

---
+++ Models/MallManageModel.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MySqlConnector;
+using Common;
+using System.ComponentModel.DataAnnotations;
+using System.Data;
+using static Common.Class1;
+
+namespace FUCOS.Models.Login
+{
+    public class MallManageModel
+    {
+
+    }
+}
