063b1c0c3d6180dc004bf6dedb95d27943a7e5d2
jongcher <jc1218a@naver.com>
2022-11-11 21:00:09 +0900

    123132

--- Controllers/MallController.cs
+++ Controllers/MallController.cs
@@ -15,7 +15,7 @@ namespace FUCOS.Controllers
             if (user_group == "null" || user_group != "Customer_USER")
             {
                 ModelState.AddModelError("경고", "사업자 및 가맹점만 열람 가능합니다.");
-                return Redirect("/"); 
+                return Redirect("/");
             }
 
             string ITEM_NM = input.ITEM_NM;
@@ -163,10 +163,10 @@ namespace FUCOS.Controllers
 
             string user_group = HttpContext.Session.GetString("SessionKeyUserGrop");
 
-            if(user_group == null) 
+            if (user_group == null)
             {
                 ModelState.AddModelError("경고", "사업자 및 가맹점만 열람 가능합니다.");
-                return Redirect("/"); 
+                return Redirect("/");
             }
 
             if (user_group == "Customer_USER")
@@ -193,10 +193,10 @@ namespace FUCOS.Controllers
             int item_count = rstQry.Rows.Count;
             int container_count = 0;
             int num = 0;
-            
-            if(item_count % 2 == 1)
+
+            if (item_count % 2 == 1)
             {
-                container_count = item_count / 2 +1;
+                container_count = item_count / 2 + 1;
             }
             else
             {
@@ -213,9 +213,9 @@ namespace FUCOS.Controllers
             List<string> list_SALE_UNIT = new List<string>();
             List<string> list_ITEM_PRICE = new List<string>();
 
-            for(int a = 0; a < rstQry.Rows.Count; a++)
+            for (int a = 0; a < rstQry.Rows.Count; a++)
             {
-                if(rstQry.Rows[a].ItemArray[0] != null)
+                if (rstQry.Rows[a].ItemArray[0] != null)
                 {
                     list_item_cd.Add(rstQry.Rows[a].ItemArray[0].ToString());
                 }
@@ -609,15 +609,56 @@ namespace FUCOS.Controllers
 
             var rstQry = comm.SelQry(conn, cmdQry, "ITEM_R_001");
 
-            if(rstQry.Rows.Count == 0)
+
+            cmdQry = new MySqlCommand();
+            comm = new CommonBase();
+            conn = comm.rtnConn();
+
+            cmdQry.Parameters.AddWithValue("P_ITEM_CD", input.ITEM_CD);
+
+            var rstQry2 = comm.SelQry(conn, cmdQry, "ITEM_OPTION_R_000");
+
+            if (rstQry.Rows.Count == 0)
             {
                 return Redirect("/Mall/franchiseeMall");
+            }
+
+            List <string> op_li_NM = new List<string>();
+            List<string> op_li_CD = new List<string>();
+            if (rstQry2.Rows.Count != 0)
+            {
+                for (int a = 0; a < rstQry2.Rows.Count; a++)
+                {
+                    string op_NM;
+                    string op_CD;
+                    if (rstQry2.Rows[a].ItemArray[1] == null)
+                    {
+                        op_NM = "";
+                    }
+                    else
+                    {
+                        op_NM = rstQry2.Rows[a].ItemArray[1].ToString();
+                    }
+
+                    if (rstQry2.Rows[a].ItemArray[0] == null)
+                    {
+                        op_CD = "";
+                    }
+                    else
+                    {
+                        op_CD = rstQry2.Rows[a].ItemArray[0].ToString();
+                    }
+                    op_li_NM.Add(op_NM);
+                    op_li_CD.Add(op_CD);
+                }
             }
 
             ViewBag.ITEM_NM = rstQry.Rows[0].ItemArray[0];
             ViewBag.UNIT_ITEM = rstQry.Rows[0].ItemArray[1];
             ViewBag.ITEM_SIZE = rstQry.Rows[0].ItemArray[2];
             ViewBag.ITEM_PRICE = Convert.ToInt32(rstQry.Rows[0].ItemArray[3]);
+            ViewBag.op_li_NM = op_li_NM;
+            ViewBag.op_li_CD = op_li_CD;
 
             return View();
         }
